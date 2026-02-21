e44ff13138279924147263275423300f437de378
Tiagozguedes <tiagozguedes@gmail.com>
2026-02-20 22:56:16 -0300

    Primeiro commit do projeto

---
+++ apirest/src/main/java/br/com/fiap/apirest/model/Produto.java
@@ -0,0 +1,66 @@
+package br.com.fiap.apirest.model;
+
+import jakarta.persistence.*;
+import org.springframework.cglib.core.Local;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUTOS") //Persistir nomes no banco de dados
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+    @Column(name = "name")
+    private String nome;
+    @Column(name = "preco")
+    private BigDecimal preco;
+    @Column(name = "expiracao")
+    private LocalDate expiracao;
+
+    public Produto(String nome, BigDecimal preco, LocalDate expiracao) {
+        this.nome = nome;
+        this.preco = preco;
+        this.expiracao = expiracao;
+    }
+
+    public Produto() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public LocalDate getExpiracao() {
+        return expiracao;
+    }
+
+    public void setExpiracao(LocalDate expiracao) {
+        this.expiracao = expiracao;
+    }
+
+
+}
---
+++ apirest/src/main/java/br/com/fiap/apirest/service/ProdutoService.java
@@ -0,0 +1,40 @@
+package br.com.fiap.apirest.service;
+
+import br.com.fiap.apirest.model.Produto;
+import br.com.fiap.apirest.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ProdutoService {
+    @Autowired //...
+    private ProdutoRepository produtoRepository;
+
+    //CRUD
+    public Produto produto(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public Produto read(UUID id) {
+        Optional<Produto> produto =  produtoRepository.findById(id);
+        return produto.orElse(null);
+    }
+
+    public List<Produto> readAll () {
+        return produtoRepository.findAll();
+    }
+
+    public Produto update(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public void delete(UUID id) {
+        produtoRepository.deleteById(id);
+    }
+
+
+}
